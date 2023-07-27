a37490a47ee2723c2ba259af41b2c267af43ff18
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-07-28 00:20:53 +0100

    individual attendance report setup

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns =[
     path('', views.MarkAttendance, name='markattend'),
     path('all/', views.ListAttendance, name='listattendance'),
-    path('staffattendance/', views.Search, name='staffattendance')
+    path('staffattendance/', views.IndividualAttendance, name='staffattendance')
 
 ]
