3896697ea2d04a23c1b7f9cdf87e5f67a9eb12ac
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-07-30 20:59:22 +0100

    report date filter

--- report/urls.py
+++ report/urls.py
@@ -4,6 +4,7 @@ from attendance.views import IndividualAttendance
 
 urlpatterns=[
     path('', views.Reportall, name='report'),
+    path('invoice/', views.GeneratePdf.as_view(), name='invoice'),
     path('single/<slug:param>', IndividualAttendance, name='singlereport')
 
 ]
--- report/views.py
+++ report/views.py
@@ -4,6 +4,11 @@ from django.contrib import messages
 from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from attendance import models
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.loader import get_template
+
+from .utils import render_to_pdf #created in step 4
 
 # Create your views here.
 
@@ -12,7 +17,6 @@ def Reportall(request):
     searchparam = request.GET.get('search')
     page = request.GET.get('page', 1)
 
-    print("aa",a)
     
     if searchparam:
         report = models.MarkAttendance.objects.filter(Q(staffid=searchparam)|Q(status=searchparam))
@@ -27,8 +31,22 @@ def Reportall(request):
     
     datefrom = request.GET.get('fromdate')
     dateto = request.GET.get('todate')
-    print("dateto", dateto)
-    # if datefrom and dateto:
-    #     report = models.MarkAttendance.objects.filter(date_signin__range=["2023-07-25", "2023-07-26"])
+    if datefrom and dateto:
+        report = models.MarkAttendance.objects.filter(date_signin__range=["2023-07-25", "2023-07-26"])
 
     return render(request, 'info/report.html', {"allreport":report})
+
+
+
+class GeneratePdf(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('info/invoice.html')
+        data = {
+             'today': datetime.now(), 
+             'amount': 39.99,
+            'customer_name': 'Cooper Mann',
+            'order_id': 1233434,
+        }
+        html = template.render({"data":data})
+        pdf = render_to_pdf('info/invoice.html', data)
+        return HttpResponse(pdf, content_type='application/pdf')
